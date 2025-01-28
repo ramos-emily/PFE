4c8c4944080d893f08db0d418703abddbbb1c577
Emily Vit�ria de Souza Ramos <42749126800@>
2025-01-28 16:35:40 -0300

    v2

---
+++ script.js
@@ -0,0 +1,26 @@
+console.log("socorro!")
+
+
+//tipos de dados
+/*
+
+    String
+    Int
+    Bollean
+
+    null
+    undefined
+*/
+
+//variaveis
+
+var x
+x = 10
+
+console.log(typeof(x))
+
+x = 'tocomsono'
+console.log(typeof(x))
+
+x = false
+console.log(typeof(x))
